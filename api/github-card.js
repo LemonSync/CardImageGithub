3187e688e4d1dac85825f7c9d2a0eb19519af7b4
Lemon <grouplemon0@gmail.com>
2025-11-02 14:24:18 +0700

    Update github-card.js

--- api/github-card.js
+++ api/github-card.js
@@ -83,9 +83,12 @@ app.get("/api/svg-card/", async (req, res) => {
   }
 
   name = name.toUpperCase();
+  desc = wrapSVGText(desc, 50, 17);
 
   const data = await fetch(`https://api.github.com/users/${name}`)
     .then(r => r.json());
+
+  name = data.name;
 
   const avatarBuffer = await fetch(data.avatar_url)
     .then(r => r.arrayBuffer())
@@ -94,85 +97,83 @@ app.get("/api/svg-card/", async (req, res) => {
   const avatarBase64 = avatarBuffer.toString("base64");
 
   const svg = `
-  <svg width="500" height="700" xmlns="http://www.w3.org/2000/svg">
-  <rect width="500" height="700" fill="#216038" rx="10" />
+  <svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">
+    <rect width="600" height="600" fill="#216038" />
 
-  <defs>
-    <clipPath id="avatarClip">
-      <circle cx="250" cy="100" r="60" />
-    </clipPath>
+    <defs>
+      <clipPath id="avatarClip" class="fide-in del-1">
+        <circle cx="300" cy="100" r="60" />
+      </clipPath>
 
-    <style>
-      .fade-in { opacity: 0; animation: fadeIn .6s forwards; }
-      .fade-up { transform-box: fill-box; transform-origin: center; opacity:0; animation: fadeInUp .6s forwards; }
-      .del-1 { animation-delay: .25s; }
-      .del-2 { animation-delay: .45s; }
-      .del-3 { animation-delay: .65s; }
-
-      @keyframes fadeIn { to { opacity: 1; } }
-      @keyframes fadeInUp {
+      <style>
+        .fade-in { opacity: 0; animation: fadeIn 0.6s forwards; }
+        .fide-up { transform-box: fill-box; transform-origin: center; opacity:0; animation: fadeInUp 0.6s forwards; }
+        .del-1 { animation-delay: 0.25s; }
+        .del-2 { animation-delay: 0.45s; }
+        .del-3 { animation-delay: 0.65s; }
+        @keyframes fadeIn { to { opacity: 1; } }
+        @keyframes fadeInUp {
         from { transform: translateY(20px); opacity: 0; }
         to { transform: translateY(0); opacity: 1; }
-      }
+        }
+        .glow {
+          animation: glowAnim 2.5s infinite ease-in-out;
+        }
+        @keyframes glowAnim {
+          0% { stroke: #22ff88; stroke-width: 1; }
+          50% { stroke: #00ffaa; stroke-width: 4; }
+          100% { stroke: #22ff88; stroke-width: 1; }
+        }
+      </style>
+    </defs>
 
-      .glow { animation: glowAnim 2.5s infinite ease-in-out; }
-      @keyframes glowAnim {
-        0% { stroke: #22ff88; stroke-width: 1; }
-        50% { stroke: #00ffaa; stroke-width: 4; }
-        100% { stroke: #22ff88; stroke-width: 1; }
-      }
-    </style>
-  </defs>
+    <image
+      class="fide-in del-1"
+      href="data:image/png;base64,${avatarBase64}"
+      x="240"
+      y="40"
+      width="120"
+      height="120"
+      clip-path="url(#avatarClip)"
+      preserveAspectRatio="xMidYMid slice"
+    />
 
-  <image
-    class="fade-in del-1"
-    href="data:image/png;base64,${avatarBase64}"
-    x="190"
-    y="40"
-    width="120"
-    height="120"
-    clip-path="url(#avatarClip)"
-    preserveAspectRatio="xMidYMid slice"
-  />
+    <circle cx="300" cy="100" r="60"
+            fill="none"
+            class="glow fide-in del-1"
+            stroke-linecap="round" />
 
-  <circle cx="250" cy="100" r="60"
-          fill="none"
-          class="glow fade-in del-1"
-          stroke="#22ff88"
-          stroke-width="1"
-          stroke-linecap="round" />
-
-  <g class="fade-up del-2">
-    <text x="250" y="200"
-          font-size="26"
-          fill="#ffffff"
-          text-anchor="middle"
-          font-family="Comic Sans MS, cursive, sans-serif"
-          text-transform="uppercase"
-          letter-spacing="6">
-      ${name}
-    </text>
-  </g>
-
-  <g class="fade-up del-3">
-    <text x="250" y="230"
-      text-anchor="middle"
+    <g transform="translate(135,56)" class="fide-up del-1">
+    <text x="300" y="200"
+      font-size="24"
       fill="#ffffff"
-      font-family="Ubuntu, sans-serif"
-      font-weight="100">
-  ${wrapSVGText(desc, 50, 20, 250)}
+      text-anchor="middle"
+      font-family="Comic Sans MS, cursive, sans-serif"
+      text-transform="uppercase"
+      letter-spacing="5">
+  ${name}
 </text>
+  </g>
+  
 
+  <g transform="translate(135,56)" class="fide-up del-2">
+    <text x="250" y="220"
+        fill="#ffffff"
+        text-anchor="middle"
+        font-family="Ubuntu, sans-serif"
+        font-weight="100">
+    ${desc}
+  </text>
   </g>
 
-  <rect x="-500" y="680" width="500" height="6" fill="#00ff88">
-    <animate attributeName="x"
-             from="-500"
-             to="500"
-             dur="1s"
-             repeatCount="indefinite" />
-  </rect>
-</svg>
+    <rect x="-500" y="580" width="400" height="6" fill="#00ff88" >
+      <animate attributeName="x"
+               from="-600"
+               to="600"
+               dur="1.5s"
+               repeatCount="indefinite" />
+    </rect>
+  </svg>
   `;
 
   res.setHeader("Content-Type", "image/svg+xml");
@@ -180,6 +181,7 @@ app.get("/api/svg-card/", async (req, res) => {
 });
 
 module.exports = app;
+
 
 
 
