5f944dd8070d422115033d0b979101c18aef1520
Lemon <grouplemon0@gmail.com>
2025-11-07 23:58:41 +0700

    Update github-card.js

--- api/github-card.js
+++ api/github-card.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 const { getGithubData } = require("../utils/githubData")
-const { generateErrorSVG, generateSVG } = require("../utils/theSystem")
+const { generateErrorSVG, generateSVG, generateSVG2 } = require("../utils/theSystem")
 const { wrapSVGText } = require("../utils/allFunction");
 
 // =========================================
@@ -16,94 +16,73 @@ app.get("/", (req, res) => {
   res.send("🚀 Server berhasil dijalankan");
 });
 
-app.get("/api/github-card", async (req, res) => {
-  const { username, desc = "Orang yang pengen jadi programmer tapi enggan ngoding", type, no, facebook, twitter } = req.query;
-
-  if (!username) {
-    return SpecialErrorMessage(res, Canvas, `Please enter the username`);
-  }
-
-
-
-  const github = await getGithubData(username);
-  const descriptionText =
-      desc && desc !== "No description provided"
-        ? desc
-        : github.desc || "Pengguna ini belum menulis bio.";
-  github.desc = descriptionText;
-
-  
-  const typeNum = parseInt(type);
-  if (isNaN(typeNum)) {
-    return SpecialErrorMessage(res, Canvas, `Invalid type format, Only 1 - 6 available`);
-  }
-
-  if (typeNum === 1) {
-    if (!no || !facebook || !twitter) {
-    return SpecialErrorMessage(res, Canvas, `Please enter the Call number, Facebook Account, and Twitter Account`);
-  }
-    
-    return typeOneFunction(res, Canvas, github.avatar_url, github.name, github.login, github.desc, twitter, facebook, no, github.totalStars, github.totalRepos, github.locate, github.follower);
-  }
-
-  if (typeNum === 2) {
-    return typeTwoFunction(res, Canvas, github.avatar_url, github.name, github.login, github.desc);
-  }
-
-  if (typeNum === 3) {
-    return typeThreeFunction(res, Canvas, github.avatar_url, github.name);
-  }
-
-  return SpecialErrorMessage(res, Canvas, `Type 1-3 only`);
-});
-
 app.get("/api/svg-card/", async (req, res) => {
 
-  let { name, desc, age, study, religion, job, number, email, hobby } = req.query;
+  let { type, name, desc, age, study, religion, job, number, email, hobby } = req.query;
 
-  if (!name || !age || !study || !religion || !job || !number || !email || !hobby) {
-  const errorSvg = generateErrorSVG(
-    "Error, Please fill in Username, Age, Study, Religion, Job, Number, Email, and Hobby.\n",
-    "The form cannot be processed before the username is filled in.",
-    "Example: /api/svg-card?name=YourName&age=YourAge&study=YourStudy&religion=YourReligion&job=YourJob&number=YourNumber&email=YourEmail&hobby=YourHobby\nParameters: name, desc(not mandatory) age, study, religion, job, number, email, hobby",
-    1150,
-    160
-  );
+  const typeNum = parseInt(type, 10);
+  if (isNaN(typeNum)) {
+    const errorSvg = generateErrorSVG(
+      "Error, Type must be a number.\n",
+      "The form cannot be processed before the type is filled in.",
+      "Example: /api/svg-card?type=2&more_parameters_here\nParameters: type (1, 2, or 3)",
+      1150,
+      160
+    );
+  }
 
-  res.setHeader("Content-Type", "image/svg+xml");
-  return res.send(errorSvg);
-}  
+  if (!type || !name) {
+    const errorSvg = generateErrorSVG(
+      "Error, Please fill in Type, and Username.\n",
+      "The form cannot be processed before the username is filled in.",
+      "Example: /api/svg-card?type=2&name=YourName&age=YourAge&study=YourStudy&religion=YourReligion&job=YourJob&number=YourNumber&email=YourEmail&hobby=YourHobby\nParameters: name, desc(not mandatory) age(not mandatory), study(not mandatory), religion(not mandatory), job(not mandatory), number(not mandatory), email(not mandatory), hobby(not mandatory)",
+      1150,
+      160
+    );
 
-/*  const github = {};
-  github.name = "Lemon";
-  github.bio = "About You And Me";
-  github.totalStars = 116;
-  github.totalRepos = 16;
-  github.totalIssues = 18;
-  github.location = "Indonesia";
-  github.totalPullRequests = 17;
-  github.followers = 47;
-*/
+    res.setHeader("Content-Type", "image/svg+xml");
+    return res.send(errorSvg);
+  }
 
-  const github = await getGithubData(name)
+  /*  const github = {};
+    github.name = "Lemon";
+    github.bio = "About You And Me";
+    github.totalStars = 116;
+    github.totalRepos = 16;
+    github.totalIssues = 18;
+    github.location = "Indonesia";
+    github.totalPullRequests = 17;
+    github.followers = 47;
+  */
+
+  const github = await getGithubData(name).catch(async (err) => {
+    const errorSvg = generateErrorSVG(
+      "Error, User not found.\n",
+      "The form cannot be processed before the username is filled in.",
+      "-",
+      1150,
+      160
+    )
+  });
 
   if (!desc) {
     desc = github.bio || "No description provided.";
   }
 
   // ==========================================
-  
-  name = github.name.toUpperCase();
-  desc = `❝ ${desc} ❞`;
-  desc = wrapSVGText(desc, 50, 17);
 
   const avatarBuffer = await fetch(github.avatar_url)
     .then(r => r.arrayBuffer())
     .then(buf => Buffer.from(buf));
-
   const avatarBase64 = avatarBuffer.toString("base64");
-  
+
+  name = github.name.toUpperCase();
+  desc = `❝ ${desc} ❞`;
+  desc = wrapSVGText(desc, 50, 17);
+
   // ==========================================
+
+  if (typeNum == 1) {
 
   const svg = await generateSVG(
     avatarBase64,
@@ -126,9 +105,34 @@ app.get("/api/svg-card/", async (req, res) => {
 
   res.setHeader("Content-Type", "image/svg+xml");
   res.send(svg);
+
+  } else if (typeNum == 2) {
+    
+    const svg2 = await generateSVG2(
+      avatarBase64,
+      name,
+      desc
+    )
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(svg2);
+  } else {
+    
+    const errorSvg = generateErrorSVG(
+      "Error, Type must be 1 or 2.\n",
+      "The form cannot be processed before the type is filled in correctly.",
+      "Example: /api/svg-card?type=2&more_parameters_here\nParameters: type (1 or 2)",
+      1150,
+      160
+    );
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(errorSvg);
+  }
 });
 
 module.exports = app;
+
 
 
 
