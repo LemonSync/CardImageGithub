81a1c379dce24def465d173e9e2cbddaefa42927
Lemon <grouplemon0@gmail.com>
2025-11-10 01:19:35 +0700

    Refactor github-card.js for improved clarity

--- api/github-card.js
+++ api/github-card.js
@@ -80,7 +80,6 @@ app.get("/api/svg-card/", async (req, res) => {
   const avatarBase64 = avatarBuffer.toString("base64");
 
   name = github.name.toUpperCase();
-  desc = wrapSVGText(desc, 50, 17);
 
   // ==========================================
 
@@ -138,6 +137,7 @@ app.get("/api/svg-card/", async (req, res) => {
 });
 
 module.exports = app;
+
 
 
 
